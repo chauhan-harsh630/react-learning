25d99454e2a71c52db6eda141102b86bb6cf6ff8
Harsh Chauhan <hc2668012@gmail.com>
2026-04-04 11:22:41 +0530

    ReduxToolKit mini Todo

---
+++ 17_ReduxToolKit/ReduxToolKit_mini/src/App.jsx
@@ -0,0 +1,12 @@
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todo />
+    </>
+  );
+}
+
+export default App;
